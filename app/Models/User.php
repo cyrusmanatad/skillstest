a047d6a8929cc79fc65b91a7561c50d9d63340da
Cyrus <c_camanatad@unilab.com.ph>
2025-03-13 17:33:57 +0800

    Update User Model
    Added property and method for admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+    
+    public function isAdmin()
+    {
+        return $this->is_admin;
     }
 }
