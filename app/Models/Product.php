01341ca46264d842e8ae542afa153ca053d69fd0
Cyrus <c_camanatad@unilab.com.ph>
2025-03-13 17:14:21 +0800

    Update Product.php
    Define the one-to-many relationship with booted method to exclude deleted product when fetching data

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
 
+    protected $fillable = ['title', 'body', 'user_id', 'del_flag'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('active', function(Builder $builder) {
+            $builder->where('del_flag', 1);
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
